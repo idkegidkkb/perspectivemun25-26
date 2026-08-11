fe0dd9b144cced4970f62dd3985e629de80b109a
shreyaas <shreyaas3k9@gmail.com>
2026-08-11 16:19:08 +0530

    Update location to Church Gate

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -140,7 +140,7 @@ const HeroSection = () => {
                 VENUE
               </p>
               <p className="font-body text-xs sm:text-base font-bold text-foreground">
-                Jai Hind College, Churchgate
+                Church Gate
               </p>
             </div>
 
