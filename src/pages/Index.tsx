82188978e979e10fa09ff77a95a17b76da5d1c69
shreyaas <shreyaas3k9@gmail.com>
2026-08-15 20:35:34 +0530

    Update page tab title to One of the Best MUNs in Mumbai

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -10,7 +10,7 @@ const Index = () => {
   return (
     <>
       <SEO 
-        title="Home | Best MUN in Mumbai" 
+        title="Home | One of the Best MUNs in Mumbai" 
         description="World Summit Model United Nations 2026 (WSMUN'26) is the premier MUN conference in Churchgate, Mumbai. Experience the best student-led diplomacy, public speaking, and youth leadership summit in Mumbai. Register now!" 
       />
       <TopNav />
